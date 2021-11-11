05ce82e65758fef17d18796cd917db96bdf4ec8c
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 21:42:24 -0700

    reformat documentation & switch to require relative

--- lib/warframe/models/news.rb
+++ lib/warframe/models/news.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'warframe/models/base'
+require_relative './base'
 
 module Warframe
   module Models
@@ -10,52 +10,52 @@ module Warframe
       # @return [String]
       attr_reader :id
 
-      # The Date the News was released
+      # The date the news was released.
       # @return [String]
       attr_reader :date
 
-      # The Image Link from the News page.
+      # The image link from the news page.
       # @return [String]
       attr_reader :image_link
 
-      # Estimated Time of Arrival.
+      # Estimated Time Until Arrival
       # @return [String]
       attr_reader :eta
 
-      # Link for more information on this News.
+      # Link for more information on this news.
       # @return [String]
       attr_reader :link
 
-      # Whether or not the News has to do with Prime Access.
+      # Whether or not the news has to do with Prime Access.
       # @return [Boolean]
       attr_reader :prime_access
       alias prime_access? prime_access
 
-      # Whether or not a DEV Stream is Available.
+      # Whether or not a DEV stream is available.
       # @return [Boolean]
       attr_reader :stream
       alias stream? stream
 
-      # Whether or not this news is regarding an update
+      # Whether or not this news is regarding an update.
       # @return [Boolean]
       attr_reader :update
       alias update? update
 
-      # Whether or not this news is of priority
+      # Whether or not this news is of priority.
       # @return [Boolean]
       attr_reader :priority
       alias priority? priority
 
-      # The entire response as a long string
+      # The entire response as a long string.
       # @return [String]
       attr_reader :as_string
 
-      # The actual message of the News
+      # The actual message of the news.
       # @return [String]
       attr_reader :message
 
-      # Available translations for the the News.
-      # @return [Hash<String, String>] Key as language code and Value as translation.
+      # Available translations for the the news.
+      # @return [OpenStruct]
       attr_reader :translations
     end
   end
