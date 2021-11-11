3a45995d1d2d890219de83f14f13b0a919a7625f
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 18:15:15 -0700

    rework nightwave_spec.rb

--- spec/models/nightwave_spec.rb
+++ spec/models/nightwave_spec.rb
@@ -3,12 +3,25 @@
 require 'rspec'
 
 RSpec.describe Warframe::Models::Nightwave do
-  json = { id: 'String' }
-  it 'Can be instantiated from JSON' do
+
+  let(:json) { load_json_file 'nightwave' }
+
+  it 'can be instantiated from JSON' do
     expect { Warframe::Models::Nightwave.new json }.to_not raise_error
   end
 
-  it 'Can access instance variables' do
-    expect(Warframe::Models::Nightwave.new(json).id).to_not be nil
+  let(:model) { Warframe::Models::Nightwave.new json }
+
+  it 'can access instance variables' do
+    expect(model.id).to_not be nil
   end
+
+  it 'can access nested attributes' do
+    expect(model.possible_challenges).to be_a Array
+  end
+
+  it 'maintains Hash structure of nested attributes' do
+    expect(model.possible_challenges[0]).to be_a Hash
+  end
+
 end
