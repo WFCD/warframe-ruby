d904424d259d7ec7eec268ad4a84cca0d239931a
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 19:35:51 -0700

    fix default options

--- lib/warframe/client_wrapper.rb
+++ lib/warframe/client_wrapper.rb
@@ -4,14 +4,15 @@ module Warframe
   # Wraps the [Warframe::Rest::Client] with necessary variables and methods
   class ClientWrapper
     BASE_URL = 'https://api.warframestat.us/'
+    DEFAULT_OPTIONS = { platform: 'pc', language: 'en' }.freeze
     attr_accessor :platform, :language
 
     # Initialize the Restful Client
     #
     # @param options [Hash]
     # @return [Warframe::Client]
-    def initialize(options = { platform: 'pc', language: 'en' })
-      options.each { |k, v| instance_variable_set "@#{k}", v }
+    def initialize(options = {})
+      DEFAULT_OPTIONS.merge(options).each { |k, v| instance_variable_set "@#{k}", v }
 
       yield self if block_given?
     end
