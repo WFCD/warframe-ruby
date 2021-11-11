e5dfbf8688293903b0d5a0b235a33bf097d6f681
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 21:39:32 -0700

    fix Base initialize method to take parsed json or json array

--- lib/warframe/models/base.rb
+++ lib/warframe/models/base.rb
@@ -5,22 +5,28 @@ require 'fast_underscore'
 module Warframe
   module Models
     # Warframe Base Model.
-    # Defaults to initialize from a [Hash],
-    # {from_array} allows for instantiation from an [Array].
+    # @param options [Hash, Array]
     class Base
       attr_reader :error, :code
 
       def initialize(options = {})
-        options.each do |k, v|
-          renamed = k.to_s.underscore
-          v = v.map { |obj| OpenStruct.new obj } if v.is_a?(Array) && v[0].is_a?(Hash)
-          instance_variable_set "@#{renamed}", v
+        if options.is_a? Array
+          from_array options
+        else
+          options.each do |k, v|
+            renamed = k.to_s.underscore
+            v = v.map { |obj| OpenStruct.new obj } if v.is_a?(Array) && v[0].is_a?(Hash)
+            v = OpenStruct.new v if v.is_a? Hash
+            instance_variable_set "@#{renamed}", v
+          end
         end
       end
 
-      # Constructs an Array of new objects
+      private
+
+      # Constructs an Array of new [Warframe::Models::Base].
       # @return [Array]
-      def self.from_array(arr)
+      def from_array(arr)
         arr.map { |obj| initialize obj }
       end
     end
