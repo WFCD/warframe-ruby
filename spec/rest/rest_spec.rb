70287c5994a61645524475a569c60d6994a0f06e
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 17:05:27 -0700

    test every route of our API

--- spec/rest/rest_spec.rb
+++ spec/rest/rest_spec.rb
@@ -5,11 +5,28 @@ RSpec.describe Warframe::REST::API do
 
   routes = Warframe::REST::API.constants
 
-  puts "Routes: #{routes}"
-  routes.each do |route|
-    describe route do
-      it 'is navigable' do
-        expect(default_client.send(route.to_s.downcase))
+  context 'Routes' do
+    routes.each do |route|
+      method = route.to_s.downcase
+      describe route do
+        it 'responds to method call' do
+          expect(default_client.send(method))
+        end
+
+        it 'properly loads data' do
+          sent = default_client.send(method)
+          model_instance = Warframe::Models.const_get(route)
+          if sent.is_a? Array
+            expect(sent.count).to be_positive
+            expect(sent[0].id).to_not be nil
+
+            sent.each do |obj|
+              expect(obj).to be_a model_instance
+            end
+          else
+            expect(sent).to be_a model_instance
+          end
+        end
       end
     end
   end
