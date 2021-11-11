996bbe9903ad0cefc51a344dfc2d67f043e56392
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-10 17:15:38 -0700

    add documentation and DataMapper

--- lib/warframe/models/nightwave.rb
+++ lib/warframe/models/nightwave.rb
@@ -4,39 +4,36 @@ require 'warframe/models/base'
 
 module Warframe
   module Models
-    # Nightwave Model
+    # Class for the response of {https://api.warframestat.us/pc/nightwave /:platform/nightwave}
     class Nightwave < Warframe::Models::Base
-      attr_accessor :id, :activation, :expiry, :reward_types, :season,
-                    :tag, :phase, :possible_challenges, :active_challenges
-      # Implement DRY-Struct
-      # attribute :id, Types::String
-      # attribute :activation, Types::Date
-      # attribute :expiry, Types::Date
-      # attribute :rewardTypes, Types::Array
-      # attribute :season, Types::Integer
-      # attribute :tag, Types::String
-      # attribute :phase, Types::Integer
-      #
-      # # Possible Challenges Currently Active
-      # attribute :possibleChallenges, Types::Array do
-      #   attribute :id, Types::String
-      #   attribute :activation, Types::Date
-      #   attribute :expiry, Types::Date
-      #   attribute :isDaily, Types::Bool
-      #   attribute :isElite, Types::Bool
-      #   attribute :desc, Types::String
-      #   attribute :reputation, Types::Integer
-      # end
-      #
-      # attribute :activeChallenges, Types::Array do
-      #   attribute :id, Types::String
-      #   attribute :activation, Types::Date
-      #   attribute :expiry, Types::Date
-      #   attribute :isDaily, Types::Bool
-      #   attribute :isElite, Types::Bool
-      #   attribute :desc, Types::String
-      #   attribute :reputation, Types::Integer
-      # end
+
+      # The ID of the Nightwave Response.
+      # @return [String]
+      property :id, String
+
+      # The Time the Mission was or will be activated.
+      # @return [String]
+      property :activation, String
+
+      # The time that the mission will expire.
+      # @return [String]
+      property :expiry, String
+
+      # The types of rewards you can get from this event.
+      # @return [Array<String>]
+      property :reward_types, Array
+
+      # The current phase of this event.
+      # @return [Integer]
+      property :phase, Integer
+
+      # List of all possible challenges.
+      # @return [Array<Hash>]
+      property :possible_challenges, Array
+
+      # Active Challenges for this event.
+      # @return [Array<Hash>]
+      property :active_challenges, Array
     end
   end
 end
