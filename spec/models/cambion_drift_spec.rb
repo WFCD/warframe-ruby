747ae534c13c05524d9746b66d1d3b69e9b8862a
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-23 17:23:40 -0500

    fix & add specs

--- spec/models/cambion_drift_spec.rb
+++ spec/models/cambion_drift_spec.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
 
+require 'rspec'
+
+RSpec.describe Warframe::Models::Alert do
+  let(:json) { load_json_file 'cambion_drift' }
+  let(:drift) { Warframe::Models::CambionDrift.new json }
+
+  it 'can be instantiated from JSON' do
+    expect { drift }.to_not raise_error
+  end
+
+  it 'can be instantiated from JSON Array' do
+    json_arr = Array.new(6, json)
+    expect { Warframe::Models::CambionDrift.new json_arr }.to_not raise_error
+  end
+
+  it 'can read the newly created objects attributes' do
+    expect(drift.active).to be_a String
+  end
+end
--- spec/models/cetus_spec.rb
+++ spec/models/cetus_spec.rb
@@ -1,1 +1,22 @@
 
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe Warframe::Models::Cetus do
+  let(:json) { load_json_file 'cetus' }
+  let(:cetus) { Warframe::Models::Cetus.new json }
+
+  it 'can be instantiated from JSON' do
+    expect { cetus }.to_not raise_error
+  end
+
+  it 'can be instantiated from JSON Array' do
+    json_arr = Array.new(6, json)
+    expect { Warframe::Models::Cetus.new json_arr }.to_not raise_error
+  end
+
+  it 'can read the newly created objects attributes' do
+    expect(cetus.day?).to be_a_boolean
+  end
+end
