b583cb4f5d7e5cac04a8d80bea9b9f3478ba7c51
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-23 17:23:43 -0500

    fix & add specs

--- lib/warframe/models/cambion_drift.rb
+++ lib/warframe/models/cambion_drift.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 require_relative './base'
+require_relative 'attributes/id'
+require_relative 'attributes/expiry'
+require_relative 'attributes/active'
 
 module Warframe
   module Models
     # Model for Cambion Drift Data {https://api.warframestat.us/pc/cambionCycle> /:platform/cambionCycle}
-    # TODO: Replace above documentation path with correct one.
     class CambionDrift < Warframe::Models::Base
-      # TODO: Add attributes
+      include Warframe::Models::Attributes::ID
+      include Warframe::Models::Attributes::Activation
+      include Warframe::Models::Attributes::Expiry
+
+      # Current active state of the world, either 'vome' or 'fass'.
+      # @return [String]
+      attr_reader :active
+
+      # The time remaining until world state switches.
+      # @return [String]
+      attr_reader :time_left
     end
   end
 end
