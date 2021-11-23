45f05f5235e559f0a3bba684f5a2bc73feb66ffe
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-23 16:50:56 -0500

    fix method and name errors

---
+++ lib/warframe/models/cetus.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative './base'
+
+module Warframe
+  module Models
+    # Model for CetusCycle Data {https://api.warframestat.us/pc/cetusCycle> /:platform/cetusCycle}
+    # TODO: Replace above documentation path with correct one.
+    class Cetus < Warframe::Models::Base
+      # TODO: Add attributes
+    end
+  end
+end
---
+++ lib/warframe/rest/api/cetus.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'warframe/models/cetus'
+require_relative '../utils'
+
+module Warframe
+  module REST
+    module API
+      # API endpoint for getting information on current Cetus data.
+      #
+      # {https://api.warframestat.us/pc/cetusCycle Example Response}
+      module Cetus
+        include Warframe::REST::Utils
+
+        # Gets the current cetusCycle Data.
+        # @return [Warframe::Models::Cetus]
+        def cetus
+          get('/cetusCycle', Warframe::Models::CetusCycle)
+        end
+      end
+    end
+  end
+end
