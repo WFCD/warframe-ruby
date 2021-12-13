05b8a524ae68e3169889b6ba38ef977a63f33c47
A.J. Romaniello <ajiellodev@gmail.com>
2021-12-12 18:27:02 -0700

    fix cache_spec

--- spec/rest/cache_spec.rb
+++ spec/rest/cache_spec.rb
@@ -1,26 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Warframe::REST:Client Cache' do
-  let(:client) { Warframe::REST::Client.new }
+  client = Warframe::REST::Client.new
   let(:old) { Warframe::Models::Nightwave.new(load_json_file('nightwave')) }
 
   it 'correctly caches object models' do
     # Create initial MOCK entry data with 1 second expiry
-    Warframe::Models::Nightwave.new_entry('/nightwave', old, 1)
-    expect(Warframe::Models::Nightwave.cache_content).to be_a Warframe::Models::Nightwave
-    expect(Warframe::Models::Nightwave.expired?).to eq false
+    client.add_to_cache('/nightwave', old, 1)
+    expect(client.get_from_cache('/nightwave')).to eq old
 
     # Should Pull from memory on new requests
-    expect(client.nightwave).to eq Warframe::Models::Nightwave.cache_content
+    expect(client.nightwave).to eq old
 
     # Wait 1 second
     sleep 1
 
-    # Cache should expire
-    expect(Warframe::Models::Nightwave.expired?).to eq true
-
     # Cache should now be set to a new response on next call
     client.nightwave
-    expect(old).to_not eq Warframe::Models::Nightwave.cache_content
+    expect(old).to_not eq client.get_from_cache('/nightwave')
   end
 end
