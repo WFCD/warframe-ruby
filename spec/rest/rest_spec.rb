49a84b40476164c6e409ad6ed3581893069ce694
A.J. Romaniello <ajiellodev@gmail.com>
2021-12-12 18:27:33 -0700

    separate rest spec by route

--- spec/rest/rest_spec.rb
+++ spec/rest/rest_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Warframe::REST::API do
-  let(:client) { Warframe::REST::Client.new }
-
+def alerts
   context '#alerts' do
     it 'does not raise error on call' do
       expect { client.alerts }.to_not raise_error
@@ -12,17 +10,9 @@ RSpec.describe Warframe::REST::API do
       expect(client.alerts).to be_a(Array || Warframe::Models::Alert)
     end
   end
+end
 
-  context '#cambion_drift' do
-    it 'does not raise error on call' do
-      expect { client.cambion_drift }.to_not raise_error
-    end
-
-    it 'properly loads data into model' do
-      expect(client.cambion_drift).to be_a Warframe::Models::CambionDrift
-    end
-  end
-
+def cetus
   context '#cetus' do
     it 'does not raise error on call' do
       expect { client.cetus }.to_not raise_error
@@ -32,7 +22,21 @@ RSpec.describe Warframe::REST::API do
       expect(client.cetus).to be_a Warframe::Models::Cetus
     end
   end
+end
 
+def cambion
+  context '#cambion_drift' do
+    it 'does not raise error on call' do
+      expect { client.cambion_drift }.to_not raise_error
+    end
+
+    it 'properly loads data into model' do
+      expect(client.cambion_drift).to be_a Warframe::Models::CambionDrift
+    end
+  end
+end
+
+def nightwave
   context '#nightwave' do
     it 'does not raise error on call' do
       expect { client.nightwave }.to_not raise_error
@@ -42,4 +46,13 @@ RSpec.describe Warframe::REST::API do
       expect(client.nightwave).to be_a Warframe::Models::Nightwave
     end
   end
+end
+
+RSpec.describe Warframe::REST::API do
+  let(:client) { Warframe::REST::Client.new }
+
+  alerts
+  cetus
+  cambion
+  nightwave
 end
